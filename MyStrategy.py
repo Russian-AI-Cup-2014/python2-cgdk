fea2262a0e2ca63277fe19356bdafbd84f688082
Yan Kalchevskiy <yan.kalchevskiy@gmail.com>
2014-09-10 01:33:05 +0700

    Added IDEA (PyCharm) type hinting.
    Включает автодополнение кода. Подробнее:
    http://www.jetbrains.com/pycharm/webhelp/type-hinting-in-pycharm.html

--- MyStrategy.py
+++ MyStrategy.py
@@ -1,9 +1,21 @@
 from math import *
+
 from model.ActionType import ActionType
+from model.Game import Game
+from model.Hockeyist import Hockeyist
+from model.Move import Move
+from model.World import World
+
 
 
 class MyStrategy:
     def move(self, me, world, game, move):
+        """
+        @type me: Hockeyist
+        @type world: World
+        @type game: Game
+        @type move: Move
+        """
         move.speed_up = -1.0
         move.turn = pi
         move.action = ActionType.STRIKE
